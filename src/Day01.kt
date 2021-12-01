287ddf269a49a588fcf7fceed3185b6d69807d78
Michael Clark <michael.clark@domo.com>
2021-12-01 16:26:27 -0700

    Day 01 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,29 @@
+data class Delta(
+        private val previousDepth: Int?,
+        private val depth: Int
+) {
+    val isDeeper: Boolean
+        get() = previousDepth?.let { p -> depth > p } ?: false
+}
+
+fun List<Int>.getDelta(index: Int) = Delta(
+        previousDepth = (index - 1).takeIf { it >= 0 }?.let { this[it] },
+        depth = this[index]
+)
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val depths = input.map { it.toInt() }
+        return IntRange(0, depths.lastIndex)
+            .map(depths::getDelta)
+            .count { it.isDeeper }
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("Day01_input")
+    println("Depth increases: ${part1(input)}")
+    //println(part2(input))
 }
