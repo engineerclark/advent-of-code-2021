08d5ce6abd5451f5d3f5f6c6164510b9d7e11ec6
Michael Clark <michael.clark@domo.com>
2021-12-01 16:50:06 -0700

    Day 01 part 2 progress

--- src/Day01.kt
+++ src/Day01.kt
@@ -6,24 +6,46 @@ data class Delta(
         get() = previousDepth?.let { p -> depth > p } ?: false
 }
 
+data class Window(
+        private val secondPrevious: Int?,
+        private val firstPrevious: Int?,
+        private val current: Int?
+) {
+    val sum: Int
+        get() = (current ?: 0) + (firstPrevious ?: 0) + (secondPrevious ?: 0)
+}
+
 fun List<Int>.getDelta(index: Int) = Delta(
-        previousDepth = (index - 1).takeIf { it >= 0 }?.let { this[it] },
+        previousDepth = this.getOrNull(index - 1),
         depth = this[index]
 )
 
+fun List<Int>.getWindow(index: Int) = Window(
+        secondPrevious = this.getOrNull(index - 2),
+        firstPrevious = this.getOrNull(index - 1),
+        current = this.getOrNull(index)
+)
+
+fun List<Int>.getWindowDelta(index: Int) = Delta(
+        previousDepth = this.getWindow(index - 1).sum,
+        depth = this.getWindow(index).sum
+)
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        val depths = input.map { it.toInt() }
-        return IntRange(0, depths.lastIndex)
-            .map(depths::getDelta)
+    fun part1(input: List<Int>): Int {
+        return IntRange(0, input.lastIndex)
+            .map(input::getDelta)
             .count { it.isDeeper }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        return IntRange(2, input.lastIndex)
+            .map(input::getWindowDelta)
+            .count { it.isDeeper }
     }
 
     val input = readInput("Day01_input")
-    println("Depth increases: ${part1(input)}")
-    //println(part2(input))
+    val depths = input.map { it.toInt() }
+    println("Part 1: Depth increases: ${part1(depths)}")
+    println("Part 2: Windowed depth increases: ${part2(depths)}")
 }
