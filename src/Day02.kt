315835947819384c37887c4189465fb23a449482
Michael Clark <michael.clark@puritynine.com>
2021-12-02 00:16:06 -0700

    Day 02 part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -3,6 +3,11 @@ data class Position(
     val horizontal: Int = 0
 )
 
+data class PositionWithAim(
+    val position: Position = Position(),
+    val aim: Int = 0
+)
+
 val positionPattern: Regex = """^(\w+) (\d+)$""".toRegex()
 
 fun stringToPosition(positionString: String): Position = positionPattern.find(positionString.trim())
@@ -22,13 +27,30 @@ fun move(startPosition: Position, delta: Position): Position = Position(
     horizontal = startPosition.horizontal + delta.horizontal
 )
 
+fun moveWithAim(startPosition: PositionWithAim, delta: Position): PositionWithAim {
+    val aim = startPosition.aim + delta.depth
+    return PositionWithAim(
+        aim = aim,
+        position = Position(
+            horizontal = startPosition.position.horizontal + delta.horizontal,
+            depth = startPosition.position.depth + (delta.horizontal * aim)
+        )
+    )
+}
+
 fun List<Position>.aggregatePosition(): Position = this.fold(Position(), ::move)
+
+fun List<Position>.aggregatePositionWithAim(): PositionWithAim = this.fold(PositionWithAim(), ::moveWithAim)
 
 fun part1(movements: List<Position>): Int = movements.aggregatePosition()
     .let { it.depth * it.horizontal }
+
+fun part2(movements: List<Position>): Int = movements.aggregatePositionWithAim()
+    .let { it.position.depth * it.position.horizontal}
 
 fun main() {
     val input = readInput("Day02_input")
     val movements = input.map(::stringToPosition)
     println("Part 1: Final positions multiplied: ${part1(movements)}")
+    println("Part 2: Final positions using aim, multiplied: ${part2(movements)}")
 }
